dfa245dbb2d5a9bcc68e18359dbde717a0a1cd32
Avenger2627 <imraninfo97@gmail.com>
2020-10-09 11:22:39 +0530

    Added:Employee 3

--- Employee.sh
+++ Employee.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -x
 
+echo "------------------------- Welcome To Employee Wage Compution ------------------------"
 
-random1=$((RANDOM%2))
+#INITIAL VALUE TO ASSING PRESENT
+isPresent=1
 
-if [ $random1 -eq 0 ]
+#TO VARIABLE
+randomNumber=$((RANDOM%2))
 
+#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
+if [ $randomNumber -eq 	$isPresent ]
 then
-	empRatePerHour=20
-	empHours=8
-	salary=$(($empRatePerHour * $empHours))
-else 
-	salary=0
-	
-fi
-echo "Salary is: " $salary
-
+	echo "Employee Present"
+  	#TO FUNCTION PART TIME EMPLOYEE AND  WAGE
+	function partTimeEmployeeAndWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+	read -p "To part time hour is :" isFullDayHour
+	calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
+}
+partTimeEmployeeAndWage
+else
+   echo "Employee Absent"
+ fi
