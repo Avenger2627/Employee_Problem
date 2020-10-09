740959e3a241d9e812608818abac0143abe357d6
Avenger2627 <imraninfo97@gmail.com>
2020-10-09 10:49:16 +0530

    Added: Present or Not

--- Employee.sh
+++ Employee.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
-echo "WELCOME TO EMPLOYEE PROGRAMS"
+#! /bin/bash -x
+
+random1=$((RANDOM%2))
+
+if [ $random1 -eq 0 ]
+
+then
+	echo " Employee is Present"
+else 
+	echo " Employee is no Present "
+fi
+
+
